21bf144b18e998d40e6aba9afa9dcd0d3265c168
LazyPhilo <118143675+EthanOwO1@users.noreply.github.com>
2025-01-07 09:00:58 -0500

    Created two more quiz types(area and patterns) and added it to the main code of the game

--- MathTrainingGame.java
+++ MathTrainingGame.java
@@ -17,26 +17,26 @@ public class MathTrainingGame{
 		// Main Screen of the Math Training Game
 		while(true){
 			con.println("Welcome to Math Training Game");
-			con.println("What would you like to do? (Play game, Leaderboard, Help, Quit) ");
+			con.println("What would you like to do? (p: play game, l: leaderboard, h: help, q: quit) ");
 			String strDecision = con.readLine();
 			
 			con.clear();
 			
 			// The Quit Screen of the Math Training Game
-			if(strDecision.equalsIgnoreCase("quit")){
+			if(strDecision.equalsIgnoreCase("q")){
 				con.println("Thank you for playing! See you next time!");
 				con.sleep(3000);
 				con.closeConsole();
 				
 			// The Leaderbaord Screen of the Math Training Game
-			} else if(strDecision.equalsIgnoreCase("leaderboard")){
+			} else if(strDecision.equalsIgnoreCase("l")){
 				con.println("This is the leaderboard");
 				con.println("Do you want to go back to main menu?");
 				String strBack = con.readLine();
 				con.clear();
 				
 			// The Help Screen of the Math Training Game
-			} else if(strDecision.equalsIgnoreCase("help")){
+			} else if(strDecision.equalsIgnoreCase("h")){
 				con.println("Welcome to Math Training Game.");
 				con.sleep(1000);
 				con.println("In this program, you will be able to choose any subject to test. ");
@@ -50,7 +50,7 @@ public class MathTrainingGame{
 				con.clear();
 				
 			// The Play Game Screen 
-			} else {
+			} else if(strDecision.equalsIgnoreCase("p")){
 				con.println("Please enter your name: ");
 				String strName = con.readLine();
 				con.clear();
@@ -58,7 +58,7 @@ public class MathTrainingGame{
 				con.println("Hi " + strName);
 				con.sleep(1000);
 				con.println("Welcome to our game! Please choose which type of quiz you would like to do?");
-				con.println("(Linear, Algebra)");
+				con.println("(Linear, Algebra, Patterns, Area)");
 				String strType = con.readLine();
 				con.clear();
 				
@@ -77,7 +77,10 @@ public class MathTrainingGame{
 						con.println(strQuestion);
 						String strResponse = con.readLine();
 					
-						if(strResponse.equalsIgnoreCase(strResponse)){
+						if(strResponse.equalsIgnoreCase(strResponse) || 
+						Integer.parseInt(strResponse) == intAnswer ||
+						Double.parseDouble(strResponse) == dblAnswer
+						){
 						
 							con.println("Congrats! You got the correct answer.");
 							intCorrectAnswer += 1;
@@ -142,9 +145,98 @@ public class MathTrainingGame{
 					con.println("Return back to main menu?");
 					String strBack = con.readLine();
 					con.clear();
+				
+				}
+			
+				// Patterns Math Practice
+				if(strType.equalsIgnoreCase("patterns")){
+					TextInputFile patterns = new TextInputFile("patterns.txt");
+					
+					while(patterns.eof() == false){
+						strQuestion = patterns.readLine();
+						intAnswer = patterns.readInt();
+						dblAnswer = patterns.readDouble();
+						strAnswer = patterns.readLine();
+						
+						con.println("Question: ");
+						con.println(strQuestion);
+						String strResponse = con.readLine();
+						
+						if(strResponse.equalsIgnoreCase(strAnswer)){
+						
+							con.println("Congrats! You got the correct answer.");
+							intCorrectAnswer += 1;
+							
+							con.sleep(3000);
+						
+						} else {
+							con.println("You got the wrong answer. It should either be: " + strAnswer + ", " + intAnswer + ", " + dblAnswer);
+							con.sleep(3000);
+						}
+						
+						intNumQuestions += 1;
+						dblPercentage = intCorrectAnswer / intNumQuestions;
+						con.clear();
+					}
+					
+					// Calculating the percentage of answers
+					con.println("Number of correct answer: " + intCorrectAnswer);
+					con.sleep(2000);
+					con.println("Thanks for playing!");
+					con.println("Return back to main menu?");
+					String strBack = con.readLine();
+					con.clear();
+				}	
+
+				// Area Math Practice
+				if(strType.equalsIgnoreCase("area")){
+					TextInputFile area = new TextInputFile("area.txt");
+					
+					while(area.eof() == false){
+						strQuestion = area.readLine();
+						intAnswer = area.readInt();
+						dblAnswer = area.readDouble();
+						strAnswer = area.readLine();
+						
+						con.println("Question: ");
+						con.println(strQuestion);
+						String strResponse = con.readLine();
+						
+						if(strResponse.equalsIgnoreCase(strAnswer)){
+						
+							con.println("Congrats! You got the correct answer.");
+							intCorrectAnswer += 1;
+							
+							con.sleep(3000);
+						
+						} else {
+							con.println("You got the wrong answer. It should either be: " + strAnswer + ", " + intAnswer + ", " + dblAnswer);
+							con.sleep(3000);
+						}
+						
+						intNumQuestions += 1;
+						dblPercentage = intCorrectAnswer / intNumQuestions;
+						con.clear();
+					}
+					
+					// Calculating the percentage of answers
+					con.println("Number of correct answer: " + intCorrectAnswer);
+					con.sleep(2000);
+					con.println("Thanks for playing!");
+					con.println("Return back to main menu?");
+					String strBack = con.readLine();
+					con.clear();
 
 				}
 			}
+			
+			else{
+				con.println("Cannot find the command");
+				con.println("Return back to main menu?");
+				String strBack = con.readLine();
+				con.clear();
+			}
+		
 		}
 	}
 }
