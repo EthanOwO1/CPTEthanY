741b92d2a899287cdd7aaa07ed0eac07830be27a
LazyPhilo <118143675+EthanOwO1@users.noreply.github.com>
2025-01-19 20:55:34 -0500

    Added visuals to the game as well as fixing minor bugs found in the programs.

--- MathTrainingGame.java
+++ MathTrainingGame.java
@@ -3,51 +3,130 @@
 // 2024-01-21
 // Version 1.1
 
+// Importing Libraries
 import arc.*;
+import java.awt.image.*;
+import java.awt.Font;
+import java.awt.Color;
+
 
 public class MathTrainingGame{	
 	
 	static String strName = "";
 	
 	public static void main(String[] args){
-		Console con = new Console("Math Training Game", 1280, 720);
+		Console con = new Console("Math Training Game", 1280, 720);		
 		
+		BufferedImage imgbackground = con.loadImage("background.png");
+		
+		con.drawImage(imgbackground, 0, 0);
+		Font font = con.loadFont("Arial Black.ttf", 28);
+		con.setTextFont(font);
+
 		// Main Screen of the Math Training Game
 		while(true){
-			con.println("Welcome to Math Training Game");
-			con.println("What would you like to do? (p: play game, l: leaderboard, h: help, q: quit) ");
+			
+			con.println();
+			con.println();
+			con.println();
+			con.println();
+
+			con.println("                                             Welcome to Math Training Game");	
+			con.println();
+			con.println("		  What would you like to do?");
+			con.println();
+			con.println("	            (p: play game, l: leaderboard, h: help, q: quit)");
+			con.println();			
+			
 			String strDecision = con.readLine();
 			
 			con.clear();
 			
 			// The Quit Screen of the Math Training Game
 			if(strDecision.equalsIgnoreCase("q")){
+				
 				con.println("Thank you for playing! See you next time!");
 				con.sleep(3000);
 				con.closeConsole();
 				
-			// The Leaderbaord Screen of the Math Training Game
+			// The Leaderboard Screen of the Math Training Game
 			} else if(strDecision.equalsIgnoreCase("l")){
 				TextInputFile leaderboard = new TextInputFile("leaderboard.txt");
 				
 				String strName = "";
 				String strTestType = "";
 				String strPercentage = "";
+				int intResults = 0;
 				
 				con.println("This is the leaderboard");
 				con.println();
-				con.println("Name		|		Test Type		|	Winning Percentage");
-				con.println("--------------------------------------------------------------------------------");
+				con.println("Name	|Test Type	|Winning Percentage");
+				con.println("------------------------------------------------------------------------------------------------");
 				
 				while(leaderboard.eof() == false){
 					strName = leaderboard.readLine();
 					strTestType = leaderboard.readLine();
 					strPercentage = leaderboard.readLine();
-					
-					con.println(strName + "		|" + strTestType + "					|" + strPercentage);
-					
+					intResults += 1;
 				}
 				
+				leaderboard.close();
+				
+				String strLeaderboard[][];
+				strLeaderboard = new String[intResults][3];
+				
+				leaderboard = new TextInputFile("leaderboard.txt");
+				
+				while(leaderboard.eof() == false){
+					
+					int intRow;
+					
+					for(intRow = 0; intRow < intResults; intRow++){
+						
+						strLeaderboard[intRow][0] = leaderboard.readLine();
+						strLeaderboard[intRow][1] = leaderboard.readLine();
+						strLeaderboard[intRow][2] = leaderboard.readLine();
+					}
+				}
+				String strNames;
+				String strTestTypes;
+				String strAccuracy;
+				int intRow;
+				int intRow2;
+				
+				// Bubble sorting
+				for(intRow2 = 0; intRow2 < intResults - 1; intRow2++){
+	
+					for(intRow = 0; intRow < intResults - 1 - intRow2; intRow++){
+						// Bubble sort. If right is bigger than left
+						
+						if(Double.parseDouble(strLeaderboard[intRow][2]) < Double.parseDouble(strLeaderboard[intRow + 1][2])){
+							
+							// Take the left item
+							strNames = strLeaderboard[intRow][0];
+							strTestType = strLeaderboard[intRow][1];
+							strAccuracy = strLeaderboard[intRow][2];
+							
+							// Right item moves to the left
+							strLeaderboard[intRow][0] = strLeaderboard[intRow + 1][0];
+							strLeaderboard[intRow][1] = strLeaderboard[intRow + 1][1];
+							strLeaderboard[intRow][2] = strLeaderboard[intRow + 1][2];
+							
+							// Put temporary value on the right
+							strLeaderboard[intRow + 1][0] = strNames;
+							strLeaderboard[intRow + 1][1] = strTestType;
+							strLeaderboard[intRow + 1][2] = strAccuracy;
+						}
+					}
+				}
+				
+				int intLoop;
+				
+				// Prints out all of the user records
+				for(intLoop = 0; intLoop < intResults; intLoop++){
+					con.println(strLeaderboard[intLoop][0] + "	|" + strLeaderboard[intLoop][1] + "	|" + strLeaderboard[intLoop][2]);
+				
+				}
 				con.println();
 				con.println("Do you want to go back to main menu?");
 				String strBack = con.readLine();
@@ -55,11 +134,17 @@ public class MathTrainingGame{
 				
 			// The Help Screen of the Math Training Game
 			} else if(strDecision.equalsIgnoreCase("h")){
-				con.println("Welcome to Math Training Game.");
+				con.println("--- Help Menu ---");
 				con.sleep(1000);
-				con.println("In this program, you will be able to choose any subject to test. ");
+				con.println("How to play: ");
 				con.sleep(1000);
-				con.println("After choosing the topic, you will have to answer questions which will be shown on the leaderboards.");
+				con.println("Choose any test type to start solving.");
+				con.sleep(1000);
+				con.println("After choosing the topic, solve all the questions.");
+				con.sleep(1000);
+				con.println("The person with the highest accuracy will be 1st in the leaderboard");
+				con.sleep(1000);
+				con.println("Try your best and aim for the top.");
 				con.sleep(1000);
 				con.println("Otherwise, have fun!!!");
 				
@@ -78,48 +163,90 @@ public class MathTrainingGame{
 				con.println("Hi " + strName);
 				con.sleep(1000);
 				con.println("Welcome to our game! Please choose which type of quiz you would like to do?");
+				
+				// All the test types that the user can choose from. Shown in the form of buttons.
+				con.setDrawColor(new Color(0, 0, 0, 50));
 
-				while(tests.eof() == false){
-					con.print(tests.readLine() + ", ");
-				}	
+				con.drawRoundRect(50, 400, 250, 100, 10, 10);
+				con.fillRoundRect(50, 400, 250, 100, 10, 10);
+
+				con.drawRoundRect(350, 400, 250, 100, 10, 10);
+				con.fillRoundRect(350, 400, 250, 100, 10, 10);
+
+				con.drawRoundRect(650, 400, 250, 100, 10, 10);
+				con.fillRoundRect(650, 400, 250, 100, 10, 10);
+
+				con.drawRoundRect(950, 400, 250, 100, 10, 10);
+				con.fillRoundRect(950, 400, 250, 100, 10, 10);
 				
 				con.println();
-				String strType = con.readLine();
-				con.clear();
-				
-				// Linear Quiz
-				if(strType.equalsIgnoreCase("linear")){
-					MathTrainingTests.linearTest(con);
-				}
-				
-				
-				// Algebra Math Practice
-				if(strType.equalsIgnoreCase("algebra")){
-					MathTrainingTests.algebraTest(con);
-				}
-			
-				// Patterns Math Practice
-				if(strType.equalsIgnoreCase("algebra")){
-					MathTrainingTests.patternsTest(con);
-				}	
+				con.println();
+				con.println();
+				con.println();
+				con.println();
+				con.println();
+				con.println();
+				con.println();
+				con.println();
 
-				// Area Math Practice
-				if(strType.equalsIgnoreCase("area")){
-					MathTrainingTests.areaTest(con);
+				
+				while(tests.eof() == false){
+					con.print("             " + tests.readLine() + "        ");
+				}
+				
+				while(true){
+					if(con.currentMouseButton() == 1) {
+						int mouseX = con.currentMouseX();
+						int mouseY = con.currentMouseY();
+
+						// Open Linear Quiz if mouse is in the designated area
+						if (mouseX >= 50 && mouseX <= 300 && mouseY >= 400 && mouseY <= 500) {
+							con.clear();
+							con.drawImage(imgbackground, 0, 0);
+							MathTrainingTests.linearTest(con);
+							break;
+							
+						}
+						// Open Algebra Quiz if mouse is in the designated area
+						if (mouseX >= 350 && mouseX <= 600 && mouseY >= 400 && mouseY <= 500) {
+							con.clear();
+							con.drawImage(imgbackground, 0, 0);
+							MathTrainingTests.algebraTest(con);
+							break;
+							
+						}
+						// Open Patterns Quiz if mouse is in the designated area
+						if (mouseX >= 650 && mouseX <= 900 && mouseY >= 400 && mouseY <= 500) {
+							con.clear();
+							con.drawImage(imgbackground, 0, 0);
+							MathTrainingTests.patternsTest(con);
+							break;
+							
+						}
+						// Open Area Quiz if mouse is in the designated area
+						if (mouseX >= 950 && mouseX <= 1200 && mouseY >= 400 && mouseY <= 500) {
+							con.clear();
+							con.drawImage(imgbackground, 0, 0);
+							MathTrainingTests.areaTest(con);
+							break;
+							
+						}
+					}
 				}
 			}
 			
+			// My Secret Menu
 			else if(strDecision.equalsIgnoreCase("Secret")){
 				con.println("Welcome to our secret menu");
 				con.println("Did you know that if we removed the wings from a fly, it becomes a walk?");
-				con.println("😂😂😂");
+				con.println("*laughs");
 				con.sleep(3000);
 				con.println("Return back to main menu?");
 				String strBack = con.readLine();
 				con.clear();
 				
 			}
-			
+			// If no commands found, return back to main menu.
 			else{
 				con.println("Cannot find the command");
 				con.println("Return back to main menu?");
--- MathTrainingTests.java
+++ MathTrainingTests.java
@@ -1,10 +1,13 @@
 import arc.*;
+import java.awt.image.*;
+
 
 public class MathTrainingTests{
 	public static void linearTest(Console con){
 		TextInputFile linear = new TextInputFile("linear.txt");
 
-		// Creating an array to randomize the questions
+
+		// Creating an array to store all of the data from the linear file
 		String strLinear[][];
 
 		// Variables and Initialization
@@ -21,6 +24,7 @@ public class MathTrainingTests{
 		// Retrieving strName from the main program
 		strName = MathTrainingGame.strName;
 
+		// Counting the number of questions 
 		while(linear.eof() == false){
 						
 			strQuestion = linear.readLine();
@@ -29,20 +33,19 @@ public class MathTrainingTests{
 			strAnswer3 = linear.readLine();
 			intQuestions += 1;
 		}
-		
 		linear.close();
-					
+		
+		// Create an array based on the number of questions
 		strLinear = new String[intQuestions][5];
-
-		System.out.println("Continue");
+		
 		
 		linear = new TextInputFile("linear.txt");
-
 		while(linear.eof() == false){
 			String strRandomNum;
 			
 			int intRow;
 			
+			// Storing all the data in the designated row/column
 			for(intRow = 0; intRow < intQuestions; intRow++){
 				
 				strRandomNum = Integer.toString((int)(Math.random() * 100 + 1));
@@ -62,48 +65,52 @@ public class MathTrainingTests{
 		String strTempAns1;
 		String strTempAns2;
 		String strTempAns3;
-		String strTempRandomNum;
+		String strRandom;
+		int intRow;
 		int intRow2;
-		int intRow3;
 	
 		
-		for(intRow3 = 0; intRow3 < intQuestions - 1; intRow3++){
+		for(intRow2 = 0; intRow2 < intQuestions - 1; intRow2++){
 			
-			for(intRow2 = 0; intRow2 < intQuestions - 1 - intRow3; intRow2++){
+			for(intRow = 0; intRow < intQuestions - 1 - intRow2; intRow++){
 				// Bubble sort. If left is bigger than right
 				
-				if(Integer.parseInt(strLinear[intRow2][4]) > Integer.parseInt(strLinear[intRow2 + 1][4])){
+				if(Integer.parseInt(strLinear[intRow][4]) > Integer.parseInt(strLinear[intRow + 1][4])){
 					
 					// Take the left item
-					strTempQuestion = strLinear[intRow2][0];
-					strTempAns1 = strLinear[intRow2][1];
-					strTempAns2 = strLinear[intRow2][2];
-					strTempAns3 = strLinear[intRow2][3];
+					strTempQuestion = strLinear[intRow][0];
+					strTempAns1 = strLinear[intRow][1];
+					strTempAns2 = strLinear[intRow][2];
+					strTempAns3 = strLinear[intRow][3];
+					strRandom = strLinear[intRow][4];
 					
 					// Right item moves to the left
-					strLinear[intRow2][0] = strLinear[intRow2 + 1][0];
-					strLinear[intRow2][1] = strLinear[intRow2 + 1][1];
-					strLinear[intRow2][2] = strLinear[intRow2 + 1][2];
-					strLinear[intRow2][3] = strLinear[intRow2 + 1][3];
+					strLinear[intRow][0] = strLinear[intRow + 1][0];
+					strLinear[intRow][1] = strLinear[intRow + 1][1];
+					strLinear[intRow][2] = strLinear[intRow + 1][2];
+					strLinear[intRow][3] = strLinear[intRow + 1][3];
+					strLinear[intRow][4] = strLinear[intRow + 1][4];
 					
 					// Put temporary value on the right
-					strLinear[intRow2 + 1][0] = strTempQuestion;
-					strLinear[intRow2 + 1][1] = strTempAns1;
-					strLinear[intRow2 + 1][2] = strTempAns2;
-					strLinear[intRow2 + 1][3] = strTempAns3;
+					strLinear[intRow + 1][0] = strTempQuestion;
+					strLinear[intRow + 1][1] = strTempAns1;
+					strLinear[intRow + 1][2] = strTempAns2;
+					strLinear[intRow + 1][3] = strTempAns3;
+					strLinear[intRow + 1][4] = strRandom;
 				}
 			}
 		}
 							
 		int intLoop;
 		
+		// Starting the test
 		for(intLoop = 0; intLoop < intQuestions; intLoop++){
 			
-			con.println(strName + "					Linear					%" + dblPercentage);
+			con.println(strName + "		Linear		" + dblPercentage + "%");
 			con.println();
 			
 			con.println("Question: ");
-			con.println(strLinear[intLoop][0]);
+			printWrappedText(con, strLinear[intLoop][0], 80);
 			String strResponse = con.readLine();
 		
 			if(strResponse.equalsIgnoreCase(strLinear[intLoop][1]) || 
@@ -113,8 +120,10 @@ public class MathTrainingTests{
 			
 				con.println("Congrats! You got the correct answer.");
 				dblCorrectAnswer += 1;
-				
+
 				con.sleep(3000);
+
+								
 			
 			} else {
 				con.println("You got the wrong answer. It should either be: " + strLinear[intLoop][1] + ", " + strLinear[intLoop][2] + ", " + strLinear[intLoop][3]);
@@ -127,7 +136,7 @@ public class MathTrainingTests{
 		}
 				
 		// Printing the name to the leaderboard
-		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt");
+		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true);
 		
 		leaderboard.println(strName);
 		leaderboard.println("Linear");
@@ -136,9 +145,9 @@ public class MathTrainingTests{
 		
 		// Displaying final results
 		con.println("Final Results: ");
-		con.println("Name:           " + strName);
-		con.println("Test type: 	 Linear");
-		con.println("Win Percentage: %" + dblPercentage);
+		con.println("Name:              " + strName);
+		con.println("Test type: 	    Linear");
+		con.println("Win Percentage: " + dblPercentage + "%");
 		
 		con.sleep(2000);
 		con.println("Thanks for playing!");
@@ -209,35 +218,38 @@ public class MathTrainingTests{
 		String strTempAns1;
 		String strTempAns2;
 		String strTempAns3;
-		String strTempRandomNum;
+		String strRandom;
+		int intRow;
 		int intRow2;
-		int intRow3;
 	
 		
-		for(intRow3 = 0; intRow3 < intQuestions - 1; intRow3++){
+		for(intRow2 = 0; intRow2 < intQuestions - 1; intRow2++){
 			
-			for(intRow2 = 0; intRow2 < intQuestions - 1 - intRow3; intRow2++){
+			for(intRow = 0; intRow < intQuestions - 1 - intRow2; intRow++){
 				// Bubble sort. If left is bigger than right
 				
-				if(Integer.parseInt(strAlgebra[intRow2][4]) > Integer.parseInt(strAlgebra[intRow2 + 1][4])){
+				if(Integer.parseInt(strAlgebra[intRow][4]) > Integer.parseInt(strAlgebra[intRow + 1][4])){
 					
 					// Take the left item
-					strTempQuestion = strAlgebra[intRow2][0];
-					strTempAns1 = strAlgebra[intRow2][1];
-					strTempAns2 = strAlgebra[intRow2][2];
-					strTempAns3 = strAlgebra[intRow2][3];
+					strTempQuestion = strAlgebra[intRow][0];
+					strTempAns1 = strAlgebra[intRow][1];
+					strTempAns2 = strAlgebra[intRow][2];
+					strTempAns3 = strAlgebra[intRow][3];
+					strRandom = strAlgebra[intRow][4];
 					
 					// Right item moves to the left
-					strAlgebra[intRow2][0] = strAlgebra[intRow2 + 1][0];
-					strAlgebra[intRow2][1] = strAlgebra[intRow2 + 1][1];
-					strAlgebra[intRow2][2] = strAlgebra[intRow2 + 1][2];
-					strAlgebra[intRow2][3] = strAlgebra[intRow2 + 1][3];
+					strAlgebra[intRow][0] = strAlgebra[intRow + 1][0];
+					strAlgebra[intRow][1] = strAlgebra[intRow + 1][1];
+					strAlgebra[intRow][2] = strAlgebra[intRow + 1][2];
+					strAlgebra[intRow][3] = strAlgebra[intRow + 1][3];
+					strAlgebra[intRow][4] = strAlgebra[intRow + 1][4];
 					
 					// Put temporary value on the right
-					strAlgebra[intRow2 + 1][0] = strTempQuestion;
-					strAlgebra[intRow2 + 1][1] = strTempAns1;
-					strAlgebra[intRow2 + 1][2] = strTempAns2;
-					strAlgebra[intRow2 + 1][3] = strTempAns3;
+					strAlgebra[intRow + 1][0] = strTempQuestion;
+					strAlgebra[intRow + 1][1] = strTempAns1;
+					strAlgebra[intRow + 1][2] = strTempAns2;
+					strAlgebra[intRow + 1][3] = strTempAns3;
+					strAlgebra[intRow + 1][4] = strRandom;
 				}
 			}
 		}
@@ -246,11 +258,11 @@ public class MathTrainingTests{
 		
 		for(intLoop = 0; intLoop < intQuestions; intLoop++){
 			
-			con.println(strName + "					Algebra	Test				%" + dblPercentage);
+			con.println(strName + "		Algebra		" + dblPercentage + "%");
 			con.println();
 			
 			con.println("Question: ");
-			con.println(strAlgebra[intLoop][0]);
+			printWrappedText(con, strAlgebra[intLoop][0], 80);
 			String strResponse = con.readLine();
 		
 			if(strResponse.equalsIgnoreCase(strAlgebra[intLoop][1]) || 
@@ -274,7 +286,7 @@ public class MathTrainingTests{
 		}	
 		
 		// Printing the results to the leaderboard
-		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt");
+		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true);
 		
 		leaderboard.println(strName);
 		leaderboard.println("Algebra");
@@ -283,9 +295,9 @@ public class MathTrainingTests{
 		
 		// Displaying final results
 		con.println("Final Results: ");
-		con.println("Name:           " + strName);
-		con.println("Test type: 	 Algebra");
-		con.println("Win Percentage: %" + dblPercentage);
+		con.println("Name:              " + strName);
+		con.println("Test type: 	    Algebra");
+		con.println("Win Percentage: " + dblPercentage + "%");
 		
 		con.sleep(2000);
 		con.println("Thanks for playing!");
@@ -357,34 +369,38 @@ public class MathTrainingTests{
 		String strTempAns2;
 		String strTempAns3;
 		String strTempRandomNum;
+		String strRandom;
+		int intRow;
 		int intRow2;
-		int intRow3;
 	
 		
-		for(intRow3 = 0; intRow3 < intQuestions - 1; intRow3++){
+		for(intRow2 = 0; intRow2 < intQuestions - 1; intRow2++){
 			
-			for(intRow2 = 0; intRow2 < intQuestions - 1 - intRow3; intRow2++){
+			for(intRow = 0; intRow < intQuestions - 1 - intRow2; intRow++){
 				// Bubble sort. If left is bigger than right
 				
-				if(Integer.parseInt(strPatterns[intRow2][4]) > Integer.parseInt(strPatterns[intRow2 + 1][4])){
+				if(Integer.parseInt(strPatterns[intRow][4]) > Integer.parseInt(strPatterns[intRow + 1][4])){
 					
 					// Take the left item
-					strTempQuestion = strPatterns[intRow2][0];
-					strTempAns1 = strPatterns[intRow2][1];
-					strTempAns2 = strPatterns[intRow2][2];
-					strTempAns3 = strPatterns[intRow2][3];
+					strTempQuestion = strPatterns[intRow][0];
+					strTempAns1 = strPatterns[intRow][1];
+					strTempAns2 = strPatterns[intRow][2];
+					strTempAns3 = strPatterns[intRow][3];
+					strRandom = strPatterns[intRow][4];
 					
 					// Right item moves to the left
-					strPatterns[intRow2][0] = strPatterns[intRow2 + 1][0];
-					strPatterns[intRow2][1] = strPatterns[intRow2 + 1][1];
-					strPatterns[intRow2][2] = strPatterns[intRow2 + 1][2];
-					strPatterns[intRow2][3] = strPatterns[intRow2 + 1][3];
+					strPatterns[intRow][0] = strPatterns[intRow + 1][0];
+					strPatterns[intRow][1] = strPatterns[intRow + 1][1];
+					strPatterns[intRow][2] = strPatterns[intRow + 1][2];
+					strPatterns[intRow][3] = strPatterns[intRow + 1][3];
+					strPatterns[intRow][4] = strPatterns[intRow + 1][4];
 					
 					// Put temporary value on the right
-					strPatterns[intRow2 + 1][0] = strTempQuestion;
-					strPatterns[intRow2 + 1][1] = strTempAns1;
-					strPatterns[intRow2 + 1][2] = strTempAns2;
-					strPatterns[intRow2 + 1][3] = strTempAns3;
+					strPatterns[intRow + 1][0] = strTempQuestion;
+					strPatterns[intRow + 1][1] = strTempAns1;
+					strPatterns[intRow + 1][2] = strTempAns2;
+					strPatterns[intRow + 1][3] = strTempAns3;
+					strPatterns[intRow + 1][4] = strRandom;
 				}
 			}
 		}
@@ -393,11 +409,11 @@ public class MathTrainingTests{
 		
 		for(intLoop = 0; intLoop < intQuestions; intLoop++){
 			
-			con.println(strName + "					Patterns Test				%" + dblPercentage);
+			con.println(strName + "		Patterns		" + dblPercentage + "%");
 			con.println();
 			
 			con.println("Question: ");
-			con.println(strPatterns[intLoop][0]);
+			printWrappedText(con, strPatterns[intLoop][0], 80);
 			String strResponse = con.readLine();
 		
 			if(strResponse.equalsIgnoreCase(strPatterns[intLoop][1]) || 
@@ -421,7 +437,7 @@ public class MathTrainingTests{
 		}	
 		
 		// Printing the results to the leaderboard
-		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt");
+		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true);
 		
 		leaderboard.println(strName);
 		leaderboard.println("Patterns");
@@ -430,9 +446,9 @@ public class MathTrainingTests{
 		
 		// Displaying final results
 		con.println("Final Results: ");
-		con.println("Name:           " + strName);
-		con.println("Test type: 	 Patterns");
-		con.println("Win Percentage: %" + dblPercentage);
+		con.println("Name:              " + strName);
+		con.println("Test type: 	    Patterns");
+		con.println("Win Percentage: " + dblPercentage + "%");
 		
 		con.sleep(2000);
 		con.println("Thanks for playing!");
@@ -441,7 +457,7 @@ public class MathTrainingTests{
 		con.clear();
 	}	
 	
-		public static void areaTest(Console con){
+	public static void areaTest(Console con){
 		TextInputFile area = new TextInputFile("area.txt");
 
 		// Creating an array to randomize the questions
@@ -476,22 +492,19 @@ public class MathTrainingTests{
 
 		System.out.println("Continue");
 		
-		area = new TextInputFile("patterns.txt");
+		area = new TextInputFile("area.txt");
 
-		while(area.eof() == false){
-			String strRandomNum;
-			
+		while(area.eof() == false){			
 			int intRow;
 			
 			for(intRow = 0; intRow < intQuestions; intRow++){
-				
-				strRandomNum = Integer.toString((int)(Math.random() * 100 + 1));
 				
 				strArea[intRow][0] = area.readLine();
 				strArea[intRow][1] = area.readLine();
 				strArea[intRow][2] = area.readLine();
 				strArea[intRow][3] = area.readLine();
-				strArea[intRow][4] = strRandomNum;
+				strArea[intRow][4] = Integer.toString((int)(Math.random() * 100 + 1));
+								
 			}
 		}
 		
@@ -503,34 +516,39 @@ public class MathTrainingTests{
 		String strTempAns2;
 		String strTempAns3;
 		String strTempRandomNum;
+		String strRandom;
+		int intRow;
 		int intRow2;
-		int intRow3;
 	
 		
-		for(intRow3 = 0; intRow3 < intQuestions - 1; intRow3++){
+		for(intRow2 = 0; intRow2 < intQuestions - 1; intRow2++){
 			
-			for(intRow2 = 0; intRow2 < intQuestions - 1 - intRow3; intRow2++){
+			for(intRow = 0; intRow < intQuestions - 1 - intRow2; intRow++){
 				// Bubble sort. If left is bigger than right
 				
-				if(Integer.parseInt(strArea[intRow2][4]) > Integer.parseInt(strArea[intRow2 + 1][4])){
+				if(Integer.parseInt(strArea[intRow][4]) > Integer.parseInt(strArea[intRow + 1][4])){
 					
 					// Take the left item
-					strTempQuestion = strArea[intRow2][0];
-					strTempAns1 = strArea[intRow2][1];
-					strTempAns2 = strArea[intRow2][2];
-					strTempAns3 = strArea[intRow2][3];
+					strTempQuestion = strArea[intRow][0];
+					strTempAns1 = strArea[intRow][1];
+					strTempAns2 = strArea[intRow][2];
+					strTempAns3 = strArea[intRow][3];
+					strRandom = strArea[intRow][4];
+
 					
 					// Right item moves to the left
-					strArea[intRow2][0] = strArea[intRow2 + 1][0];
-					strArea[intRow2][1] = strArea[intRow2 + 1][1];
-					strArea[intRow2][2] = strArea[intRow2 + 1][2];
-					strArea[intRow2][3] = strArea[intRow2 + 1][3];
+					strArea[intRow][0] = strArea[intRow + 1][0];
+					strArea[intRow][1] = strArea[intRow + 1][1];
+					strArea[intRow][2] = strArea[intRow + 1][2];
+					strArea[intRow][3] = strArea[intRow + 1][3];
+					strArea[intRow][4] = strArea[intRow + 1][4];
 					
 					// Put temporary value on the right
-					strArea[intRow2 + 1][0] = strTempQuestion;
-					strArea[intRow2 + 1][1] = strTempAns1;
-					strArea[intRow2 + 1][2] = strTempAns2;
-					strArea[intRow2 + 1][3] = strTempAns3;
+					strArea[intRow + 1][0] = strTempQuestion;
+					strArea[intRow + 1][1] = strTempAns1;
+					strArea[intRow + 1][2] = strTempAns2;
+					strArea[intRow + 1][3] = strTempAns3;
+					strArea[intRow + 1][4] = strRandom;
 				}
 			}
 		}
@@ -539,11 +557,11 @@ public class MathTrainingTests{
 		
 		for(intLoop = 0; intLoop < intQuestions; intLoop++){
 			
-			con.println(strName + "					Patterns Test				%" + dblPercentage);
+			con.println(strName + "		Area		" + dblPercentage + "%");
 			con.println();
 			
 			con.println("Question: ");
-			con.println(strArea[intLoop][0]);
+			printWrappedText(con, strArea[intLoop][0], 80);
 			String strResponse = con.readLine();
 		
 			if(strResponse.equalsIgnoreCase(strArea[intLoop][1]) || 
@@ -566,7 +584,7 @@ public class MathTrainingTests{
 		}	
 		
 		// Printing the results to the leaderboard
-		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt");
+		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true);
 		
 		leaderboard.println(strName);
 		leaderboard.println("Area");
@@ -575,9 +593,9 @@ public class MathTrainingTests{
 		
 		// Displaying final results
 		con.println("Final Results: ");
-		con.println("Name:           " + strName);
-		con.println("Test type: 	 Area");
-		con.println("Win Percentage: %" + dblPercentage);
+		con.println("Name:              " + strName);
+		con.println("Test type: 	    Area");
+		con.println("Win Percentage: " + dblPercentage + "%");
 		
 		con.sleep(2000);
 		con.println("Thanks for playing!");
@@ -585,6 +603,25 @@ public class MathTrainingTests{
 		String strBack = con.readLine();
 		con.clear();
 	}
+	
+	    public static void printWrappedText(Console con, String text, int maxLineWidth) {
+        String[] words = text.split(" ");
+        StringBuilder currentLine = new StringBuilder();
+
+        for (String word : words) {
+			
+            // Check if adding the next word would exceed the maximum line width
+            if (currentLine.length() + word.length() + 1 > maxLineWidth) {
+                con.println(currentLine.toString().trim()); 
+                currentLine = new StringBuilder();
+            }
+            currentLine.append(word).append(" "); 
+        }
+ 
+        if (currentLine.length() > 0) {
+            con.println(currentLine.toString().trim());
+        }
+    }
 	
 }
 	
